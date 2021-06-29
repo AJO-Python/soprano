d08629022554fd021f40728e8f42c93303b8f44a
AJO-python <josh.owen@stfc.ac.uk>
2021-06-29 15:37:50 +0100

    Docstrings, refactoring, and tests for data/nmr

--- soprano/data/nmr.py
+++ soprano/data/nmr.py
@@ -40,8 +40,13 @@ except IOError:
     _nmr_data = None
 
 
-def _get_nmr_data():
-
+def _get_nmr_data() -> dict:
+    """
+    Helper method to ensure the proper NMR data is loaded from
+    soprano/data/nmrdata.json
+    :return: Data on NMR relevant properties of elements and isotopes
+        - spin, gyromagnetic ratio and quadrupole moment
+    """
     if _nmr_data is not None:
         return _nmr_data
     else:
@@ -49,36 +54,59 @@ def _get_nmr_data():
                            'wrong with this installation of Soprano')
 
 
-def _get_isotope_data(elems, key, isotopes={}, isotope_list=None,
-                      use_q_isotopes=False):
+def _get_isotope_data(elems, key, isotopes=None, isotope_list=None,
+                      use_q_isotopes=False) -> list:
+    """
+    Provides isotope_data about isotopes from soprano/isotope_data/nmrdata.json
+    TODO: Add support for multiple isotopes of same element
+        Is this covered by using isotope_list instead?
+
+    :param elems: Elements to get isotope_data for
+    :type elems: Union(list[str] | str)
+    :param key: Value to retrieve - ["gamma", "I", "Q"]
+    :type key: str
+    :param isotopes: Elements with their corresponding isotopes Z numbers
+    :type isoptopes: dict
+    :param isotope_list: Ordered list of isotopes corresponding to elems, optional
+    :type isotope_list: list
+    :param use_q_isotopes: Whether to use the quadrupole
+    :type use_q_isotopes: bool
+    :return: Isotope isotope_data for the requested elements and parameters
+    :rtype: list
+    """
 
     if isinstance(elems, str):
-        elems = [elems] # It's a single element
+        elems = [elems]  # It's a single element
 
-    data = np.zeros(len(elems))
+    isotope_data = np.zeros(len(elems))
     nmr_data = _get_nmr_data()
-
-    for i, e in enumerate(elems):
-
-        if e not in nmr_data:
+    for i, element in enumerate(elems):
+        if element not in nmr_data:
             # Non-existing element
-            raise RuntimeError('No NMR data on element {0}'.format(e))
+            raise RuntimeError('No NMR data on element {0}'.format(element))
 
-        iso = nmr_data[e]['iso']
-        if use_q_isotopes and nmr_data[e]['Q_iso'] is not None:
-            iso = nmr_data[e]['Q_iso']
-        if e in isotopes:
-            iso = isotopes[e]
-        if isotope_list is not None and isotope_list[i] is not None:
-            iso = isotope_list[i]
+        element_data = nmr_data[element]
+        # Uses most common isotope if not otherwise specified
+        iso_Z = element_data['iso']
+        # Refactored to use elifs - in soprano/properties/nmr/efg docstrings explain
+        # precedence. Having "if" instead of "elif" did not respect this priority
+        # Uses the quadrupole isotope as the Z number
+        if use_q_isotopes and element_data['Q_iso'] is not None:
+            iso_Z = element_data['Q_iso']
+        # Uses the isotopes parameter if its set
+        elif (isotopes is not None) and (element in isotopes):
+            iso_Z = isotopes[element]
+        # Uses the isotope list parameter if its set
+        elif isotope_list is not None and isotope_list[i] is not None:
+            iso_Z = isotope_list[i]
 
         try:
-            data[i] = nmr_data[e][str(iso)][key]
+            # Retrieve requested isotope parameter to return
+            isotope_data[i] = element_data[str(iso_Z)][key]
         except KeyError:
             raise RuntimeError('Data {0} does not exist for isotope {1} of '
-                               'element {2}'.format(key, iso, e))
-
-    return data
+                               'element {2}'.format(key, iso_Z, element))
+    return isotope_data
 
 
 def _el_iso(sym):
@@ -87,10 +115,9 @@ def _el_iso(sym):
     """
 
     nmr_data = _get_nmr_data()
-
     match = re.findall('([0-9]*)([A-Za-z]+)', sym)
     if len(match) != 1:
-        raise ValueError('Invalid isotope symbol')
+        raise ValueError(f'Invalid isotope symbol - Use a single element, not a formula')
     elif match[0][1] not in nmr_data:
         raise ValueError('Invalid element symbol')
 
@@ -105,64 +132,74 @@ def _el_iso(sym):
     return el, iso
 
 
-def nmr_gamma(el, iso=None):
+def nmr_gamma(elems, iso=None):
     """Gyromagnetic ratio for an element
+    N.B This cannot handle a system with the different isotopes of the same element
+        dict keys being the element means the isotopes overload each other and only
+        one dict entry is made
+        - Is this ever going to be an edge case or can we ignore it?
 
     Return the gyromagnetic ratio for the given element and isotope, in
     rad/(s*T)
 
     | Args:
-    |   el (str):   element symbol
-    |   iso (int):  isotope. Default is the most abundant one.
+    |   elems (str|list(str)):  element symbol(s)
+    |   iso (int|list(int)):  isotopes. Default is the most abundant one.
 
     | Returns:
     |   gamma (float):  gyromagnetic ratio in rad/(s*T)    
     """
+    if isinstance(iso, int) or isinstance(iso, str):
+        iso = [iso]  # It's a single element
+    if isinstance(elems, str):
+        elems = [elems]  # It's a single element
+    if iso:
+        if len(iso) != len(elems):
+            raise ValueError("Elements and isotopes dimension mismatch")
+    return _get_isotope_data(elems, 'gamma', isotope_list=iso)
 
-    isotopes = {}
-    if iso is not None:
-        isotopes[el] = iso
 
-    return _get_isotope_data([el], 'gamma', isotopes=isotopes)
-
-
-def nmr_spin(el, iso=None):
+def nmr_spin(elems, iso=None):
     """Nuclear spin for an element
 
     Return the nuclear spin for the given element and isotope, in
     Bohr magnetons
 
     | Args:
-    |   el (str):   element symbol
+    |   elems (str):  element symbol
     |   iso (int):  isotope. Default is the most abundant one.
 
     | Returns:
     |   I (float):  nuclear spin in Bohr magnetons
     """
+    if isinstance(iso, int) or isinstance(iso, str):
+        iso = [iso]  # It's a single element
+    if isinstance(elems, str):
+        elems = [elems]  # It's a single element
+    if iso:
+        if len(iso) != len(elems):
+            raise ValueError("Elements and isotopes dimension mismatch")
+    return _get_isotope_data(elems, 'I', isotope_list=iso)
 
-    isotopes = {}
-    if iso is not None:
-        isotopes[el] = iso
 
-    return _get_isotope_data([el], 'I', isotopes=isotopes)
-
-
-def nmr_quadrupole(el, iso=None):
+def nmr_quadrupole(elems, iso=None):
     """Quadrupole moment for an element
 
     Return the quadrupole moment for the given element and isotope, in
     barns
 
     | Args:
-    |   el (str):   element symbol
+    |   elems (str):   element symbol
     |   iso (int):  isotope. Default is the most abundant one.
 
     | Returns:
     |   Q (float):  quadrupole moment in barns
     """
-
-    isotopes = {}
-    if iso is not None:
-        isotopes[el] = iso
-
-    return _get_isotope_data([el], 'Q', isotopes=isotopes)
+    if isinstance(iso, int) or isinstance(iso, str):
+        iso = [iso]  # It's a single element
+    if isinstance(elems, str):
+        elems = [elems]  # It's a single element
+    if iso:
+        if len(iso) != len(elems):
+            raise ValueError("Elements and isotopes dimension mismatch")
+    return _get_isotope_data(elems, 'Q', isotope_list=iso)
--- tests/data_tests.py
+++ tests/data_tests.py
@@ -13,10 +13,29 @@ import os
 import unittest
 import numpy as np
 from ase.data import vdw_radii as vdw_radii_ase
-from soprano.data import (vdw_radius, nmr_gamma, nmr_spin, nmr_quadrupole)
+from soprano.data import (vdw_radius,
+                          nmr_gamma,
+                          nmr_spin,
+                          nmr_quadrupole,
+                          )
+from soprano.data.nmr import (_get_nmr_data,
+                              _get_isotope_data,
+                              _el_iso,
+                             )
 
 
 class TestData(unittest.TestCase):
+
+    def test_get_nmr_data(self):
+        output = _get_nmr_data()
+        self.assertEqual(type(output), dict)
+        # Sanity check that the json loaded has correct Muon data
+        self.assertIn("Mu", output)
+        self.assertEqual(output["Mu"]["1"]["gamma"], -851615463.149621)
+        from soprano.data.nmr import _nmr_data
+        # Check that the import fails if the file cannot be loaded
+        # _nmr_data = None
+        # self.assertRaises(RuntimeError, lambda: _get_nmr_data)
 
     def test_vdw(self):
 
@@ -25,19 +44,115 @@ class TestData(unittest.TestCase):
         self.assertEqual(vdw_radius('C', 'ase'), vdw_radii_ase[6])
 
     def test_gamma(self):
-
+        """nmr_gamma, nmr_spin, nmr_quadrupole can all be refactored into a single function with arg "key"=("gamma"|"I"|"Q")
+        """
+        # Single values
         self.assertEqual(nmr_gamma('H'), 267522128.0)
         self.assertEqual(nmr_gamma('H', 2), 41066279.1)
 
-    def test_spin(self):
+        # Array inputs
+        gamma_list = nmr_gamma(['H', 'I'], [1, 127])
+        self.assertEqual(gamma_list[0], 267522128.0)
+        self.assertEqual(gamma_list[1], 53895730.0)
 
+        # Error handling for mismatched element and isotope dims
+        self.assertRaises(ValueError, lambda: nmr_gamma(['H', 'S'], [2]))
+
+    def test_spin(self):
         self.assertEqual(nmr_spin('H'), 0.5)
         self.assertEqual(nmr_spin('H', 2), 1)
 
     def test_quadrupole(self):
-
         self.assertEqual(nmr_quadrupole('H'), 0)
         self.assertEqual(nmr_quadrupole('H', 2), 2.86)
+
+    def test_get_isotope_data(self):
+        """TODO: Refactor to remove the isotope: dict arg. Isotope_list does the same
+        thing but allows for multiple isotopes of the same element
+        I think it is redundant but its used in a lot of places so will leave for now
+        """
+        H_iso1_gamma = 267522128.0
+        H_iso2_gamma = 41066279.1
+        # Basic functionality
+        data = _get_isotope_data(elems='H',
+                                 key='gamma',
+                                 isotopes=None,
+                                 isotope_list=None,
+                                 use_q_isotopes=True)
+        self.assertIsNotNone(data)
+        # Element does not exist
+        self.assertRaises(RuntimeError, _get_isotope_data,
+                          elems='NotAnElement',
+                          key='gamma',
+                          isotopes=None,
+                          isotope_list=None,
+                          use_q_isotopes=False)
+        # One element of list does not exist
+        self.assertRaises(RuntimeError, _get_isotope_data,
+                          elems=['NotAnElement', 'H'],
+                          key='gamma',
+                          isotopes=None,
+                          isotope_list=None,
+                          use_q_isotopes=False)
+        # Incorrect key requested
+        self.assertRaises(RuntimeError, _get_isotope_data,
+                          elems=['H', 'H'],
+                          key='fake_key',
+                          isotopes=None,
+                          isotope_list=None,
+                          use_q_isotopes=True)
+        # Isotopes as a dictionary
+        data = _get_isotope_data(elems='H',
+                                 key='gamma',
+                                 isotopes={'H': 2},
+                                 isotope_list=None,
+                                 use_q_isotopes=False)
+        self.assertEqual(data[0], H_iso2_gamma)
+        # Isotope as an ordered list
+        data = _get_isotope_data(elems=['H', 'H'],
+                                 key='gamma',
+                                 isotopes=None,
+                                 isotope_list=[2, 2],
+                                 use_q_isotopes=False)
+        self.assertEqual(data[0], H_iso2_gamma)
+        # Multiple isotopes of the same element (isotope_list)
+        data = _get_isotope_data(elems=['H', 'H'],
+                                 key='gamma',
+                                 isotopes=None,
+                                 isotope_list=[1, 2],
+                                 use_q_isotopes=False)
+        self.assertEqual(data[0], H_iso1_gamma)
+        self.assertEqual(data[1], H_iso2_gamma)
+        # Use Q isotopes
+        data = _get_isotope_data(elems=['H', 'H'],
+                                 key='gamma',
+                                 isotopes=None,
+                                 isotope_list=None,
+                                 use_q_isotopes=True)
+        self.assertEqual(data[0], H_iso2_gamma)
+        # Prioritise Q_iso over isotope_list (Q_iso=2 for H)
+        data = _get_isotope_data(elems=['H', 'H'],
+                                 key='gamma',
+                                 isotopes=None,
+                                 isotope_list=[1, 1],
+                                 use_q_isotopes=True)
+        self.assertEqual(data[0], H_iso2_gamma)
+
+    def test_el_iso(self):
+        # Default isotope used
+        element, isotope = _el_iso('H')
+        self.assertEqual(element, 'H')
+        self.assertEqual(isotope, '1')
+        # Request specific isotope
+        element, isotope = _el_iso('3H')
+        self.assertEqual(element, 'H')
+        self.assertEqual(isotope, '3')
+        # Giving more than one element
+        self.assertRaises(ValueError, _el_iso, '1H1H')
+        # Element not in database
+        self.assertRaises(ValueError, _el_iso, 'NotAnElement')
+        # Isotope doesnt exist
+        self.assertRaises(ValueError, _el_iso, '5H')
 
 if __name__ == '__main__':
     unittest.main()
